e72e21b5b8b6bd18dba186d40abc71dbb85fffea
firstfu <firefirstfu@gmail.com>
2023-09-15 16:37:43 +0800

    chore

---
+++ gitSync.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+## Git同步script
+
+echo "+++++++++++++++++++++++ Start +++++++++++++++++++++++"
+
+echo "---- docker start ----"
+git fetch --all
+# origin/master 是你需要同步的分支，這裡是同步遠端分支master
+git reset --hard origin/master
+git pull
+
+echo "+++++++++++++++++++++++ END +++++++++++++++++++++++"
