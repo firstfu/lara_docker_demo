6bdd5df1de22124820a49bd4c12a205c2682bbf0
firstfu <firefirstfu@gmail.com>
2023-09-25 14:58:25 +0800

    chore

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
-    @vite(['resources/js/app.js'])
+    {{-- @vite('resources/css/app.css') --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
