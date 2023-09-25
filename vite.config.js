5f3ee7a457380c9a10392c06f02f1014cfafe3ab
firstfu <firefirstfu@gmail.com>
2023-09-25 16:18:21 +0800

    chore

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import path from "path";
 
+console.log(process.env?.APP_NAME);
+
 export default defineConfig({
     plugins: [
         laravel({
@@ -13,8 +15,7 @@ export default defineConfig({
         host: "0.0.0.0",
         port: "5178",
         hmr: {
-            host: "0.0.0.0",
-            // host: "159.223.74.248",
+            host: "159.223.74.248",
             port: "5178",
         },
     },
