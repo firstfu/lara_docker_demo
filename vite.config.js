a20bc4f1a53b278b1a48be2e726a758c80b51cb7
firstfu <firefirstfu@gmail.com>
2023-09-25 16:03:11 +0800

    chore

--- vite.config.js
+++ vite.config.js
@@ -10,13 +10,8 @@ export default defineConfig({
         }),
     ],
     server: {
-        // host: "0.0.0.0",
-        // port: "5178",
-        // host: true,
-        // 這上這個配置，才可以在外部訪問server上的css和js+hmr
-        // hmr: {
-        //     host: "159.223.74.248",
-        // },
+        host: "0.0.0.0",
+        port: "5178",
     },
     resolve: {
         alias: {
