9f13d9d74ac95fabb4387a486fe46da8611f1213
firstfu <firefirstfu@gmail.com>
2023-09-25 16:22:12 +0800

    chore

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,8 @@ export default defineConfig({
         host: "0.0.0.0",
         port: "5178",
         hmr: {
-            host: "159.223.74.248",
+            // host: "159.223.74.248",
+            host: "0.0.0.0",
             port: "5178",
         },
     },
